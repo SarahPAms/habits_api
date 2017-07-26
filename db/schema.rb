4ecc0b55046b2df948a5e80322a4422e2045844a
SarahPAms <Sarah.L.Polan@gmail.com>
2017-07-26 19:52:32 +0200

    add models

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170724165051) do
+ActiveRecord::Schema.define(version: 20170724180847) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,6 +51,17 @@ ActiveRecord::Schema.define(version: 20170724165051) do
     t.index ["frequency_id"], name: "index_habits_on_frequency_id", using: :btree
   end
 
+  create_table "profiles", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "image"
+    t.text     "bio"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_profiles_on_user_id", using: :btree
+  end
+
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
@@ -71,4 +82,5 @@ ActiveRecord::Schema.define(version: 20170724165051) do
   add_foreign_key "categories", "goals"
   add_foreign_key "goals", "habits", column: "habits_id"
   add_foreign_key "habits", "frequencies"
+  add_foreign_key "profiles", "users"
 end
