102da7075ed1b6def2af36f817b3b696155b549f
SarahPAms <Sarah.L.Polan@gmail.com>
2017-09-16 17:31:00 +0200

    add GET and GET ALL

---
+++ app/controllers/api/circle_of_fulfillment_controller.rb
@@ -0,0 +1,18 @@
+class Api::CircleOfFulfillmentController < ApplicationController
+
+  def index
+    render status: 200, json: {
+      categories: Category.all
+    }.to_json
+  end
+
+  def show
+    category = Category.find(params[:id])
+
+    render status: 200, json: {
+      category: category
+    }.to_json
+  end
+
+
+end
