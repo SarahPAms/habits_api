4529414b9b6e01a980ecd412bddf3bbff3b7e212
SarahPAms <Sarah.L.Polan@gmail.com>
2017-09-17 11:48:12 +0200

    circle of life update

--- app/controllers/api/circle_of_fulfillment_controller.rb
+++ app/controllers/api/circle_of_fulfillment_controller.rb
@@ -1,5 +1,5 @@
 class Api::CircleOfFulfillmentController < ApplicationController
-
+  skip_before_filter :verify_authenticity_token
   def index
     render status: 200, json: {
       categories: Category.all
@@ -14,5 +14,24 @@ class Api::CircleOfFulfillmentController < ApplicationController
     }.to_json
   end
 
+  def update
+    category = Category.find(params[:id])
+    if category.update(category_params)
+      render status: 201, json: {
+        message: "Thanks for keeping us in the loop",
+        category: category
+      }.to_json
+    else
+      render status: 422, json: {
+        message: "Sorry, we didn't quite get that",
+        errors: category.errors
+      }.to_json
+    end
+  end
+
+  private
+    def category_params
+      params.require(:category).permit(:title, :score, :goal_id)
+    end
 
 end
